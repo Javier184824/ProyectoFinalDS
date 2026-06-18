06ecb132843bc3cb3b3b91d696e7c8156b2aa791
Javier184824 <leejavier62@gmail.com>
2026-06-17 22:25:04 -0600

    Update config.php

--- public/config.php
+++ public/config.php
@@ -7,12 +7,12 @@
 define('DB_HOST',    'localhost');
 define('DB_NAME',    'sistema_entregas');
 define('DB_USER',    'admin');
-define('DB_PASS',    'password');
+define('DB_PASS',    'passwordFor1admin');
 define('DB_CHARSET', 'utf8mb4');
 
 // --- URL base de la aplicacion (sin trailing slash) ---
 // Cambia BASE_PATH si la app vive en una subcarpeta de Apache
-define('BASE_URL',  'http://localhost/sistema_entregas');
+define('BASE_URL',  'http://170.9.50.197:80');
 define('BASE_PATH', '/sistema_entregas');
 
 // --- API externa de geolocalizacion ---
