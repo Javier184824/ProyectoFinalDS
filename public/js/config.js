83057528a57af37e49e30d4260f5232b0b1f9b74
Javier184824 <leejavier62@gmail.com>
2026-06-17 23:21:29 -0600

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -4,7 +4,7 @@
 // reglas: API_BASE no debe terminar en barra
 
 // vm de oracle controlada por javier
-window.API_BASE = 'http://170.9.26.131:80';
+window.API_BASE = 'http://170.9.50.197:80';
 
 // local en este equipo (xampp), ajusta la subcarpeta si difiere
 // window.API_BASE = 'http://localhost/sistema_entregas';
